20ec913874696d8ad9c9b66a5c4cb8a43d3eef42
Aaron Ackerman <theron17@gmail.com>
2012-08-16 07:23:33 -0500

    whitespace cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,38 @@
 $(document).ready(function(){
-		twttr.anywhere(function (T) {
-			var currentUser,
-			screenName,
-			profileImage,
-			screenNameTemp,
-			profileImageTag;
+  twttr.anywhere(function (T) {
+    var currentUser,
+        screenName,
+        profileImage,
+        screenNameTemp,
+        profileImageTag;
 
-			if (T.isConnected()) {
-				currentUser = T.currentUser;
-				screenName = currentUser.data('screen_name');
-				profileImage = currentUser.data('profile_image_url');
-				profileImageTag = "<img src='" + profileImage + "'/>";
-				screenNameTemp = '<a class="logged-sn">'+screenName+'</a><div class="followers"></div>';
-				$(".twitter-login").append(profileImageTag + screenNameTemp);
-				TF.init(screenName);
-			} else {
-				T(".twitter-login").connectButton({
-					authComplete: function(user) {
-						currentUser = user;
-						screenName = currentUser.data('screen_name');
-						profileImage = currentUser.data('profile_image_url');
-						profileImageTag = "<img src='" + profileImage + "'/>";
-						screenNameTemp = '<a class="logged-sn">'+screenName+'</a><div class="followers"></div>';
-						$(".twitter-login").append(profileImageTag + screenNameTemp);
-						$('.twitter-login iframe').remove();
-						TF.init(screenName);
-					}
-				});
-			}
-		});
-		$('.sn-button').bind('click', function(){
-			var screenName = $('.sn-input').val();
-			TF.init(screenName);
-			$('.sn-button').fadeOut();
-			$('.sn-input').fadeOut();
-		});
+    if (T.isConnected()) {
+      currentUser = T.currentUser;
+      screenName = currentUser.data('screen_name');
+      profileImage = currentUser.data('profile_image_url');
+      profileImageTag = "<img src='" + profileImage + "'/>";
+      screenNameTemp = '<a class="logged-sn">'+screenName+'</a><div class="followers"></div>';
+      $(".twitter-login").append(profileImageTag + screenNameTemp);
+      TF.init(screenName);
+    } else {
+      T(".twitter-login").connectButton({
+        authComplete: function(user) {
+          currentUser = user;
+          screenName = currentUser.data('screen_name');
+          profileImage = currentUser.data('profile_image_url');
+          profileImageTag = "<img src='" + profileImage + "'/>";
+          screenNameTemp = '<a class="logged-sn">'+screenName+'</a><div class="followers"></div>';
+          $(".twitter-login").append(profileImageTag + screenNameTemp);
+          $('.twitter-login iframe').remove();
+          TF.init(screenName);
+        }
+      });
+    }
+  });
+  $('.sn-button').bind('click', function(){
+    var screenName = $('.sn-input').val();
+    TF.init(screenName);
+    $('.sn-button').fadeOut();
+    $('.sn-input').fadeOut();
+  });
 });
